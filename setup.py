b7dda4a590aba2485c72386c7c052e8607e2f046
Devon Meunier <devon@ajah.ca>
2014-08-25 09:21:06 -0400

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='coding-challenge-backend-d',
+    description='An application to concurrently process images.',
+    install_requires=['Pillow'],
+)
