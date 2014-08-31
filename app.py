d0802570619b1dbfa9f9d5aca22a44f60aaee0e0
Philippe Caya <philippe.caya@gmail.com>
2014-08-31 10:29:29 -0400

    Serial functional reference of the image processor (load, scale, blur, crop, save)

--- app.py
+++ app.py
@@ -5,10 +5,19 @@ import os
 from PIL import Image, ImageFilter
 
 
+SOURCE_FOLDER = './images'
+DESTINATION_FOLDER = './processed_images'
+
+
 def images():
-    image_dir = os.walk('./images').next()
+    image_dir = os.walk(SOURCE_FOLDER).next()
     for filename in image_dir[2]:
-        yield open(os.path.join(image_dir[0], filename), 'rb')
+        name = os.path.splitext(filename)[0]
+        yield name, open(os.path.join(image_dir[0], filename), 'rb')
+
+
+def processed_filename(name):
+    return DESTINATION_FOLDER + '/' + name + '.jpg'
 
 
 class BusbudBanner(object):
@@ -60,11 +69,36 @@ class BusbudBanner(object):
         """Crop `image` to `height` pixels from the middle."""
         y = image.size[1]
         offset = (y - height) / 2
-        return name + '-vmiddle', cls.crop_verticall(image, offset, y - offset)
+        return name + '-vmiddle', cls.crop_vertical(image, offset, y - offset)
+
+
+def serial_image_processor(name, fp):
+    """ Create a simple implementation of the required
+        image processing tasks, we will use this as a
+        functional reference"""
+    name, image = BusbudBanner.load(name, fp)
+    print "image bands %s" % str(image.getbands())
+    print "scale_x %s" % name
+    name, image = BusbudBanner.scale_x(name, image)
+    print "blur %s" % name
+    name, image = BusbudBanner.blur(name, image)
+    # bottom crop
+    print "crop_bottom %s" % name
+    name_bottom, image_bottom = BusbudBanner.crop_bottom(name, image)
+    print "save %s" % name_bottom
+    BusbudBanner.save(processed_filename(name_bottom), image_bottom)
+    # top crop
+    name_top, image_top = BusbudBanner.crop_top(name, image)
+    BusbudBanner.save(processed_filename(name_top), image_top)
+    # vmiddle crop
+    name_vmiddle, image_vmiddle = BusbudBanner.crop_vmiddle(name, image)
+    BusbudBanner.save(processed_filename(name_vmiddle), image_vmiddle)
 
 
 def main():
-    raise NotImplementedError
+    for name, image in images():
+        print "processing %s image %s" % (name, image)
+        serial_image_processor(name, image)
 
 
 if __name__ == '__main__':
